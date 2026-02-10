44be561829bfb00b9aae58f250a38bfd932b1836
Pankaj kumar <pankaj@Pankajs-MacBook-Pro.local>
2026-02-10 15:21:48 +0530

    First commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listen on port http:/localhost:${PORT}`)
+})
